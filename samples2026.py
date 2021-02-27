d2cf75d92e6df24a1d13c6e2cb5cee30087950fe
mthakorecern <mitanshu.thakore@cern.ch>
2021-02-27 13:38:13 +0100

    Updated

--- samples2026.py
+++ samples2026.py
@@ -1,20 +1,20 @@
-path2026 = "/home/mthakore/PROD_2_0/" #reduced JES
+path2026 = "/scratchssd/thakore/PROD_2_0/" #reduced JES
 
 samples={
 
-"vbfHmm_powheg" : {"xsec": 0.0008210722 , "files": ["/home/mthakore/PROD_2_0/vbfHmm_powheg_merged.root"] }, #xsec need to be set later on with actual values
+"vbfHmm_powheg" : {"xsec": 0.0008210722 , "files": ["/scratchssd/thakore/PROD_2_0/vbfHmm_powheg_merged.root"] }, #xsec need to be set later on with actual values
 
-"GluGluHToMuMu_powheg" : {"xsec": 0.0008210722 , "files": ["/home/mthakore/PROD_2_0/GluGluHToMuMu_powheg_merged.root"] }, #xsec need to be set later on with actual values
+"GluGluHToMuMu_powheg" : {"xsec": 0.0008210722 , "files": ["/scratchssd/thakore/PROD_2_0/GluGluHToMuMu_powheg_merged.root"] }, #xsec need to be set later on with actual values
 
-"DYToLL_madgraphMLM" : {"xsec": 32.95, "files":["/home/mthakore/PROD_2_0/DYToLL_madgraphMLM_merged.root"] },
+"DYToLL_madgraphMLM" : {"xsec": 32.95, "files":["/scratchssd/thakore/PROD_2_0/DYToLL_madgraphMLM_merged.root"] },
 
-"EWKZ2Jets_ZToLL_madgraph" : {"xsec": 4.38 , "files": ["/home/mthakore/PROD_2_0/EWKZ2Jets_ZToLL_madgraph_merged.root"] },
+"EWKZ2Jets_ZToLL_madgraph" : {"xsec": 4.38 , "files": ["/scratchssd/thakore/PROD_2_0/EWKZ2Jets_ZToLL_madgraph_merged.root"] },
 
-"DYJets_MLL_50_madgraphMLM" : {"xsec": 5711, "files": ["/home/mthakore/PROD_2_0/DYJets_MLL_50_madgraphMLM_merged.root"] }, 
+"DYJets_MLL_50_madgraphMLM" : {"xsec": 5711, "files": ["/scratchssd/thakore/PROD_2_0/DYJets_MLL_50_madgraphMLM_merged.root"] }, 
 
-"DYJetsToLL_M_100_madgraphMLM" : {"xsec": 246.5 , "files": ["/home/mthakore/PROD_2_0/DYJetsToLL_M_100_madgraphMLM_merged.root"] },
+"DYJetsToLL_M_100_madgraphMLM" : {"xsec": 246.5 , "files": ["/scratchssd/thakore/PROD_2_0/DYJetsToLL_M_100_madgraphMLM_merged.root"] },
 
-"TT_Tune_powheg" : {"xsec": 864.4, "files": ["/home/mthakore/PROD_2_0//TT_Tune_powheg_merged.root"] }
+"TT_Tune_powheg" : {"xsec": 864.4, "files": ["/scratchssd/thakore/PROD_2_0/TT_Tune_powheg_merged.root"] }
 }
 
 ## Add "files" automatically if not defined
